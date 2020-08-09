1cfff3ead2c0d9e8ba2464972839796cec5e92e8
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-08-09 21:49:38 +0600

    theme done

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,33 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
+const subLink = document.getElementById('sub-link');
+const bar = document.querySelector('.bar');
+
+// theme
+const theme = document.querySelector('.theme');
+const orca = document.querySelector('.orca');
+const theBlueLagoon = document.querySelector('.theBlueLagoon');
+const deepSeaSpace = document.querySelector('.deepSeaSpace');
 
 hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
+});
+
+
+// Theme
+theme.addEventListener("click", () => {
+    subLink.classList.toggle("active");
+});
+
+bar.addEventListener("click", () => {
+    subLink.classList.remove("active");
+});
+orca.addEventListener("click", () => {
+    container.classList.toggle("orca");
+});
+theBlueLagoon.addEventListener("click", () => {
+    container.classList.toggle("theBlueLagoon");
+});
+deepSeaSpace.addEventListener("click", () => {
+    container.classList.toggle("deepSeaSpace");
 });
