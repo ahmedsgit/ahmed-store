a49a0089b44b05186265a20152703b5b1a24c2c1
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-08-10 14:44:14 +0600

    login registration validation

--- resources/script.js
+++ resources/script.js
@@ -10,22 +10,120 @@ const orca = document.querySelector('.orca');
 const theBlueLagoon = document.querySelector('.theBlueLagoon');
 const deepSeaSpace = document.querySelector('.deepSeaSpace');
 
-// Login Registration
+
+/********* Login & Registration ********/
+
 const loginBtn = document.getElementById('login');
-const popupLogin = document.getElementById('popup-login');
 const registrationBtn = document.getElementById('registration');
+const popupLogin = document.getElementById('popup-login');
 const popupRegistration = document.getElementById('popup-registration');
-const closeRegistrationBtn = document.getElementById('close-registration');
 const closeLoginBtn = document.getElementById('close-login');
+const closeRegistrationBtn = document.getElementById('close-registration');
+
+/*---------------Registration ID------------ */
+const regUsername = document.getElementById('reg-username');
+const regEmail = document.getElementById('reg-email');
+const regPassword = document.getElementById('reg-password');
+const regConPassword = document.getElementById('reg-con-password');
+/*---------------Login ID------------ */
+const loginUsername = document.getElementById('log-username');
+const loginPassword = document.getElementById('log-password');
+
+// Form error 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+
+}
+// Form successful
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// get Field name by id
+function getFieldName(input) {
+    return input.id.charAt(4).toUpperCase() + input.id.slice(5);
+}
+
+// Empty field validation
+function checkEmpty(inputArr) {
+    inputArr.forEach((input) => {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required.`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters.`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check valid Email
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else if (regEmail.value.trim() === '') {
+        showError(input, 'Email is Required.')
+    } else {
+        showError(input, 'Email is not valid.');
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(password, conPassword) {
+    if (password.value !== conPassword.value) {
+        showError(conPassword, 'Password do not match');
+    } else {
+        showSuccess(conPassword);
+    }
+}
+
+// Main Registration function
+function registration(e) {
+    e.preventDefault();
+    checkLength(regUsername, 3, 15);
+    checkEmail(regEmail);
+    checkLength(regPassword, 6, 25);
+    checkEmpty([regUsername, regPassword]);
+    checkPasswordsMatch(regPassword, regConPassword);
+}
+
+function checkValidUser(loginArr) {
+    loginArr.forEach((input, index) => {
+        if ((index == 0 && input.value !== 'ahmed') || (index == 1 && input.value !== '1234')) {
+            showError(input, 'Username or Password Incorrect.');
+        }
+    });
+}
+
+// main Login function
+function login(e) {
+    e.preventDefault();
+    checkValidUser([loginUsername, loginPassword]);
+    checkEmpty([loginUsername, loginPassword]);
+}
 
 
-
+// Main page
 hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
 });
 
 
-// Theme
+/******* Themes *********/
 theme.addEventListener("click", () => {
     subLink.classList.toggle("active");
 });
@@ -62,3 +160,6 @@ registrationBtn.addEventListener("click", () => {
 closeLoginBtn.addEventListener("click", () => {
     popup.style.display = "none";
 });
+
+popupRegistration.addEventListener('submit', registration);
+popupLogin.addEventListener('submit', login);
